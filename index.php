657ee50f825e0e1cea7e7d04c4795bc73a9348ed
ThibFr33 <thibs.cha@gmail.com>
2025-08-08 13:33:45 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/style/style.css">
+    <title>Task-crud</title>
+</head>
+<body>
+    <main>
+        
+    </main>
+    
+</body>
+</html>
